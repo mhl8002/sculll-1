aa3cebfa12fe174eb3bce7b6f46288bda4af6eec
mhl8002 <mhl8002@126.com>
2017-07-31 15:54:32 +0800

    add register dev

--- scull.c
+++ scull.c
@@ -2,6 +2,8 @@
 #include <linux/kernel.h>
 #include <linux/init.h>
 #include <linux/sched.h>
+#include <linux/fs.h>
+#include <linux/cdev.h>
 //#include <linux/list.h>
 //#include <linux/mm.h>
 //#include <linux/mm_types.h>
@@ -11,31 +13,45 @@ MODULE_LICENSE("Dual BSD/GPL");
 MODULE_AUTHOR("mhl ");
 MODULE_DESCRIPTION("test ");
 
-/*
- * print the module information
- */
-static void print_module(void)
-{
-    struct module *mod;
+unsigned int sculll_major=0;
+unsigned int sculll_minor=0;
+dev_t dev=0;
+struct cdev cdev;
 
-    printk(KERN_ALERT "this module: %p==%p\n", &__this_module, THIS_MODULE);
-    printk(KERN_ALERT "module state: %d\n", THIS_MODULE->state);
-    printk(KERN_ALERT "module name: %s\n", THIS_MODULE->name);
+static struct file_operations sculll_fops={
+    .owner = THIS_MODULE,
 
-    list_for_each_entry(mod, *(&THIS_MODULE->list.prev), list);
-    printk(KERN_ALERT "module name: %s\n", mod->name);
-    printk(KERN_ALERT "module state: %d\n", THIS_MODULE->state);
-}
-
+};
 
 static int scull_init(void)
 {
-    print_module( );
+    dev_t dn;
+    if(alloc_chrdev_region(&dev,sculll_minor,4,"scullhm"))
+    {
+        printk(KERN_ALERT "register chrdev region is fail");
+    }
+    else
+    {
+        sculll_major = MAJOR(dev);
+        printk(KERN_ALERT "register chrdev region success,sculll-major= %d",sculll_major);
+    }
+
+    dn = MKDEV(sculll_major,sculll_minor);
+    cdev_init(&cdev,&sculll_fops);
+    cdev.owner = THIS_MODULE;
+    cdev.ops = &sculll_fops;
+    if(cdev_add(&cdev,dn,4))
+    {
+        printk(KERN_ALERT "cdev add error");
+    }
+    else
+    {
+        printk(KERN_ALERT "cdev add success");
+    }
 
     printk(KERN_ALERT "run in cpu %d\n", get_cpu());
 
     printk(KERN_ALERT "PAGE_OFFSET : 0x%lx, TASK_SIZE : 0x%lx", PAGE_OFFSET, TASK_SIZE);
-
 
     return 0;
 }
@@ -44,6 +60,7 @@ static int scull_init(void)
 
 static void scull_exit(void)
 {
+    unregister_chrdev_region(dev,4);
     printk(KERN_ERR"exit");
 }
 
